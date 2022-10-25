63fec386b51259aa5bb0dca31b2101140bba5466
Maxim <110911222+Maxeroz@users.noreply.github.com>
2022-10-25 12:17:05 +0500

    Destructuring objects

--- index.js
+++ index.js
@@ -8,5 +8,24 @@ let num2 = 3;
 let example = `${word1}
 ${word2}
 `
-console.log(example);
+// console.log(example);
 document.getElementById("example").innerText = example;
+
+// Destructuring objects
+
+const player = {
+    name: "Lebron James",
+    club: "LA Lakers",
+    address: {
+        city: "Los Angeles"
+    }
+
+}
+
+console.log( player.address.city)
+
+const { name, club, address: { city }} = player;
+
+console.log(`${name} plays for ${club}`);
+
+console.log(`${name} lives in ${city}`);
