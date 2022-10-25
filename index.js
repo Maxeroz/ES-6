5f336e9e4a75a2311f3ec188e9a6acf395bb60bf
Maxim <110911222+Maxeroz@users.noreply.github.com>
2022-10-25 11:52:09 +0500

    Template Literals

---
+++ index.js
@@ -0,0 +1,12 @@
+// Template Literals
+
+let word1 = "Maxim";
+let word2 = "Ozerskii";
+let num1 = 2;
+let num2 = 3;
+
+let example = `${word1}
+${word2}
+`
+console.log(example);
+document.getElementById("example").innerText = example;
